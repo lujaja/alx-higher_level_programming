85e832594ae1f9f8b858688f0f17cbcaf9c8aaf8
lujaja <jarzcyber@gmail.com>
2023-08-13 10:15:44 +0300

    my python script update

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,13 +1,8 @@
 #!/use/bin/python3
+# 0-print_list_integer.py
 # Lujaja Luvuga
 
 def print_list_integer(my_list=[]):
-    """write a function that prints all integers of a list
-    one int per line
-    dont import any module
-    assume the list only contains ints
-    you are not allowed to cast integers into string
-    you have to use str.format() to print integers
-    """
-    for num in range(len(my_list)):
-        print("{:d}".format(my_list[num]))
+    """Print all integers of a list."""
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
