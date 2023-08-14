8dabd41731df9dbe13a01334fd90e576b0b2d6d8
lujaja <jarzcyber@gmail.com>
2023-08-14 12:39:17 +0300

    my python script update

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+/**
+ * is_palindrome - function to check if a singly linked list is
+ * a palindrome
+ *
+ * @head: pointer pointer to head node
+ * Return: 1 if is a palindrome, 0 if it`s not
+ *
+ * Description: c function to check if a singley linked list is a palindrom
+ */
+int is_palindrome(listint_t **head)
+{
+	if (!(*head))
+		return (1);
+
+	listint_t *temp, *prev_node, *curr_node, *h;/* *t, *hh;*/
+	/* step one: make a copy and reverse the copy */
+	temp = h = *head;
+	prev_node = temp;
+	curr_node = temp->next;
+	temp = temp->next;
+
+	prev_node->next = NULL;
+
+	while (temp)
+	{
+		temp = temp->next;
+		curr_node->next = prev_node;
+		prev_node = curr_node;
+		curr_node = temp;
+	}
+	temp = prev_node;
+	/* step two: compare the reverse list and the copy of the original list*/
+	while (h)
+	{
+		if (h->n != temp->n)
+			return (0);
+		/*t = temp;
+		hh = h;*/
+		h = h->next;
+		temp = temp->next;
+		/*free(t);
+		free(hh);*/
+	}
+		
+	return (1);
+}
