f9540dff31edc9048026ebc05783c942f8850459
lujaja <jarzcyber@gmail.com>
2023-08-20 13:38:34 +0300

    python scripts

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@
 
 def multiple_returns(sentence):
     """function to return tuple with lenth of a strin"""
-    if (len(sentence) == 0):
+    if (sentence[0] == None):
         return (None)
     lst = [len(sentence), sentence[0]]
     return tuple(lst)
