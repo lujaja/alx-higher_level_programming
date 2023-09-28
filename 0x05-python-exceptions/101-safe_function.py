024b56f563837f04e1faf7d56ff3bfb6135c6d96
lujaja <jarzcyber@gmail.com>
2023-09-28 13:54:18 +0300

    exceptions update

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Lujaja Luvuga <jarzcyber@gmail.com>
+
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        return (fct(*args))
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
