b2820fd9bba18ac2e34ee5aa834573d7b440a628
lujaja <jarzcyber@gmail.com>
2023-09-28 10:13:10 +0300

    exceptions update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
         my_list (list): the list ot print elements from.
         x: number of elements to print:
 
-    Return: 
+    Return:
         Number of elements printed.
     """
     ret = 0
