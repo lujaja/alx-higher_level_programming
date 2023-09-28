0c8e2b9ff5cb7ba2b46d1051dfca3e0cc392b20f
lujaja <jarzcyber@gmail.com>
2023-09-28 11:37:37 +0300

    exceptions update

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# 3-safe_print_division.py
+# Lujaja Luvuga <jarzcyber@gmail.com>
+
+def safe_print_division(a, b):
+    """ Divides two integrs and prints the result
+
+    Args:
+        a: parameter
+        b: parameter
+
+    Return:
+        The value of the division, otherwise None.
+    """
+
+    try:
+        div = a / b
+    except (TypeError, ZeroDivisionError, ValueError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return (div)
