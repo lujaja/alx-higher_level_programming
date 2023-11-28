d2c3093cf5976d34ed86768380db7d71f246a2b3
lujaja <jarzcyber@gmail.com>
2023-11-28 09:36:41 +0300

    update

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,10 @@
 --This script displays top 3 of cities temp during july and august
+CREATE TABLE iF NOT EXISTS j_a
+	SELECT *
+	FROM temperatures
+	WHERE month = 7 OR month = 8;
 SELECT city, AVG(value) as avg_temp
-FROM temperatures
-WHERE month = 7 OR month = 8
+FROM a_j
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
