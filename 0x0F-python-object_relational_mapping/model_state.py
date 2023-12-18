81bd626a247115737ad20a089c0bb0f3d883e102
lujaja <jarzcyber@gmail.com>
2023-12-18 20:20:03 +0300

    ORM update

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,5 +10,5 @@ Base = declarative_base()
 
 class State(Base):
     __tablename__ = 'state'
-    id = Column(Integer, autoincrement=True, unique=True, primary_key=True)
-    name = Column(String(128), nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
