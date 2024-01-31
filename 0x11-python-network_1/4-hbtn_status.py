a2daa507e53b95e1dbc90af59c4b486c791dfa30
lujaja <jarzcyber@gmail.com>
2024-01-31 13:38:11 +0300

    update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,8 +6,7 @@ import requests
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
-    response = requests.get(url)
-    if response.status_code == 200:
-        res = response.text
+    r = requests.get(url)
+    if r.status_code == 200:
         print("Body response:\n\t- type: {}\n\t- content: {}".format
-              (type(res), "OK"))
+              (type(str(r)), r.text))
