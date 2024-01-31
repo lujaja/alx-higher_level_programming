3571c3e4dc6779e662c892f4983fccc821adfa21
lujaja <jarzcyber@gmail.com>
2024-01-31 14:44:05 +0300

    update

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,5 +7,10 @@ import requests
 
 if __name__ == "__main__":
     r = requests.get(argv[1])
-    headers = r.headers
-    print(str(headers['X-Request-Id']))
+    if r.status_code == 200:
+        for key, value in r.headers.items():
+            if key == "X-Request-Id":
+                print("{}".format(value))
+                break
+    else:
+        print(f"Error: {r.status_code} - {r.reason}")
