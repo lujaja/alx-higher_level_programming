cf07f127cc57305c72d1f4972831bd36c099b8ec
lujaja <jarzcyber@gmail.com>
2024-01-31 13:52:10 +0300

    update

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-# Value of variable X-Request-Id in headers
+"""
+Value of variable X-Request-Id in headers
+"""
 from sys import argv
 import requests
 
 if __name__ == "__main__":
-    response = requests.get(argv[1])
-    header = response.headers
-    print(header['X-Request-Id'])
+    r = requests.get(argv[1])
+    headers = r.headers
+    print(str(headers['X-Request-Id']))
