50a83766f568de09050a7b875054319b966c62c5
lujaja <jarzcyber@gmail.com>
2024-01-31 12:51:43 +0300

    update

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-# Display the value of X-Request-Id
+"""
+Display the value of X-Request-Id
+"""
 from sys import argv
-import urllib.request
+from urllib.request import urlopen
 
 if __name__ == "__main__":
     url = argv[1]
-    with urllib.request.urlopen(url) as response:
+    with urlopen(url) as response:
         print(response.headers['X-Request-Id'])
