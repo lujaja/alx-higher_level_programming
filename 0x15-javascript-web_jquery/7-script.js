5a692fb014e24402e73b2ba18357cb9e9f61c188
lujaja <jarzcyber@gmail.com>
2024-02-28 23:22:08 +0300

    jQuery

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,7 @@
+$(document).ready(() => {
+    $.getJSON("https://swapi-api.alx-tools.com/api/people/5/?format=json",
+        (data) => {
+            $("#character").text(data.name)            
+        }
+    );
+})
